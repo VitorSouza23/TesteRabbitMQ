b12227644e0c388fdc6a116f75624d77ccf7e307
Vitor de Souza <vitor.souza@ndd.com.br>
2020-10-19 16:47:47 -0300

    Commit inicial

---
+++ Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel model = connection.CreateModel())
+                {
+                    model.QueueDeclare(queue: "Teste", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+                    EventingBasicConsumer consumer = new EventingBasicConsumer(model);
+                    consumer.Received += (model, ea) =>
+                    {
+                        byte[] body = ea.Body.ToArray();
+                        string message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(message);
+                    };
+
+                    model.BasicConsume(queue: "Teste", autoAck: true, consumer: consumer);
+                    Console.WriteLine("Consumer funcionando...");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory() { HostName = "localhost" };
+            using (IConnection connection = connectionFactory.CreateConnection())
+            {
+                using (IModel model = connection.CreateModel())
+                {
+                    model.QueueDeclare(queue: "Teste", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    string message = "Teste de mensagem RabbitMQ";
+                    byte[] body = Encoding.UTF8.GetBytes(message);
+
+                    model.BasicPublish(exchange: string.Empty, routingKey: "Teste", basicProperties: null, body: body);
+                    Console.WriteLine("Mensagem enviada.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
