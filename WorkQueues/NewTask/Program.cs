7637722c8c5e1663e872033e2dffd786c132eb9f
Vitor de Souza <vitor.souza@ndd.com.br>
2020-11-12 18:18:16 -0300

    Adicioando teste de WorkQueues

---
+++ WorkQueues/NewTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    using static System.Console;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel model = connection.CreateModel();
+
+            model.QueueDeclare(
+                queue: "task_queue",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            string message = GetMessage(args);
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            model.BasicPublish(
+                exchange: string.Empty,
+                routingKey: "task_queue",
+                basicProperties: null,
+                body: body);
+
+            WriteLine("Mensagem enviada.");
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Teste de tasks");
+        }
+    }
+}
---
+++ WorkQueues/Worker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    using static System.Console;
+
+    class Program
+    {
+        static void Main()
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel model = connection.CreateModel();
+
+            model.QueueDeclare(
+                queue: "teste",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+            );
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(model);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                WriteLine($"Mensagem recebida: {message}");
+
+                int dots = message.Split('.').Length - 1;
+                Thread.Sleep(dots * 1000);
+
+                WriteLine("[x] Feito.");
+            };
+
+            model.BasicConsume(
+                queue: "task_queue",
+                autoAck: true,
+                consumer: consumer
+            );
+
+            WriteLine("Esperando por mensagens...");
+            ReadLine();
+        }
+    }
+}
