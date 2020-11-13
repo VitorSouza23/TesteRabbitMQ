c93ad4b27bb13f6ec22444deb1086469b6bb6ce8
Vitor de Souza <vitor.souza@ndd.com.br>
2020-11-13 11:04:18 -0300

    Adição de publicador e inscrito

---
+++ PublishSubscribe/Publisher/Program.cs
@@ -0,0 +1,42 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace Publish
+{
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: "log", type: ExchangeType.Fanout);
+
+            string message = GetMessage(args);
+            byte[] body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(
+                exchange: "logs",
+                routingKey: string.Empty,
+                basicProperties: null,
+                body: body
+            );
+
+            WriteLine($"[x] Enviado: {message}");
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+               ? string.Join(" ", args)
+               : "info: Hello World!");
+        }
+    }
+}
---
+++ PublishSubscribe/Subscribe/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Subscribe
+{
+    using System.Text;
+    using RabbitMQ.Client;
+    using RabbitMQ.Client.Events;
+    using static System.Console;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using IConnection connection = connectionFactory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(
+                exchange: "logs",
+                type: ExchangeType.Fanout
+            );
+
+            string queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(
+                queue: queueName,
+                exchange: "logs",
+                routingKey: string.Empty
+            );
+
+            WriteLine("[*] Esperando por logs...");
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                WriteLine($" [x] {message}");
+            };
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: consumer
+            );
+
+            WriteLine("Acarque qualquer tecla para sair...");
+            ReadKey();
+        }
+    }
+}
